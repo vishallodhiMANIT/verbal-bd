2f7732c7d381cabf79659b068d2da10b46a0c5c7
vishallodhiMANIT <vishallodhi896@gmail.com>
2025-07-23 18:22:45 +0530

    Update redis.js

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,10 +2,10 @@ const { createClient }  = require('redis');
 
 const redisClient = createClient({
     username: 'default',
-    password: process.env.REDIS_PASS,
+    password: 'YjHqzKb3hPZ0OEQYTdka7Iq3eeaUJ3t4',
     socket: {
-        host: 'redis-19934.c212.ap-south-1-1.ec2.redns.redis-cloud.com',
-        port: 19934
+         host: 'redis-13964.c301.ap-south-1-1.ec2.redns.redis-cloud.com',
+        port: 13964
     }
 });
 
